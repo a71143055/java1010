29e5fbc89d4a242d2f0fccd6f414012820bebd64
a71143055 <a71143055@gmail.com>
2024-10-10 09:32:08 +0900

    java1010

--- src/chp09/ArrayListTest.java
+++ src/chp09/ArrayListTest.java
@@ -1,4 +1,20 @@
 package chp09;
 
+import java.util.ArrayList;
+
 public class ArrayListTest {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Kim");
+        names.add("Park");
+        names.add("Lee");
+        names.add("Jeong");
+
+        names.set(1, "Cha");
+
+        String removedName = names.remove(2);
+
+        System.out.println("삭제된 요소 : " + removedName);
+    }
 }
